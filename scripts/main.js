fc5963ceb8ae3ac4320fdd58323567237c673fc5
Solar <Nicholasmrnickmorris@gmail.com>
2018-06-07 09:01:52 -0400

    added a body for designs

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-let content = document.getElementsByClassName("content");
-let fullscreen = document.getElementsByClassName("fullscreen")[0];
+let content = document.getElementsByClassName("logo-wrapper");
+let fullscreenLogo = document.getElementsByClassName("fullscreen-logo")[0];
 
-fullscreen.addEventListener("click",function(e){
+fullscreenLogo.addEventListener("click",function(e){
     e.target.classList.remove("visible");
 });
 
 for(var i = 0; i < content.length; i++){
     content[i].addEventListener('click',function(e){
-        fullscreen.classList.add("visible");
-        fullscreen.children[0].src = e.target.src;
+        fullscreenLogo.classList.add("visible");
+        fullscreenLogo.children[0].src = e.target.src;
     });
 }
