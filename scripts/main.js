21a4d6066cdd1c2f80305935ef643f76e89fdd86
Solar <Nicholasmrnickmorris@gmail.com>
2018-06-07 10:25:17 -0400

    collections change

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 let content = document.getElementsByClassName("logo-wrapper");
 let fullscreenLogo = document.getElementsByClassName("fullscreen-logo")[0];
+var $wrap = $("#wrap");
+
 
 fullscreenLogo.addEventListener("click",function(e){
     e.target.classList.remove("visible");
@@ -11,3 +13,37 @@ for(var i = 0; i < content.length; i++){
         fullscreenLogo.children[0].src = e.target.src;
     });
 }
+
+
+
+
+
+$wrap.on("click", ".page-link", function(evt){
+    evt.preventDefault();
+    
+    
+    if(window.location === this.href)
+        return;
+    
+
+    var pageTitle = (this.title) ? this.title : this.textContent;
+        pageTitle = (this.getAttribute("rel") === "home") ? pagetitle: pageTitle + " - Alazar Aklilu";
+    
+    History.pushState(null,pageTitle,this.href);
+    
+});
+
+
+
+History.Adapter.bind(window, "statechange",function(){
+    var state = History.getState();
+    $.get(state.url,function(res){
+        $.each($(res), function(index,elem){
+            if($wrap.selector !== "#" + elem.id){
+                return;
+            }
+            $wrap.html($(elem).html());
+        })
+        
+    })
+});
